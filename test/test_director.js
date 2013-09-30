2b5824fe0a4cb7bc3912ff93999768bc7b00988b
Jonathan Lipps <jlipps@gmail.com>
2013-09-30 14:28:28 +0100

    incorporated monocle's parallelism

--- test/test_director.js
+++ test/test_director.js
@@ -2,6 +2,8 @@
 
 var yiewd = require('yiewd')
   , monocle = require('monocle-js')
+  , o_O = monocle.o_O
+  , ll = monocle.ll
   , run = monocle.run
   , sleep = monocle.utils.sleep
   , Drums  = require('../lib/drums')
@@ -25,26 +27,35 @@ var drumScore = (function() {
 var pianoScore = 'c4 d8 e8 f8 g8 a8 b8 C4 b8 a8 g8 f8 e8 d8 ';
 pianoScore = pianoScore + pianoScore + 'c4';
 
-run(function*() {
-  var pianoDriver = yiewd.remote('localhost', 4723);
-  var drumsDriver = yiewd.remote('localhost', 4725);
-  yield pianoDriver.init(caps);
-  yield drumsDriver.init(caps);
-  var drums = new Drums(drumsDriver);
-  var piano = new Piano(pianoDriver);
-  yield drums.chooseFromMainMenu();
-  yield sleep(0.6);
+var initPiano = o_O(function*(driver, piano) {
+  yield driver.init(caps);
   yield piano.chooseFromMainMenu();
   yield sleep(0.6);
   yield piano.chooseOctave(4);
   yield sleep(0.5);
+});
+
+var initDrums = o_O(function*(driver, drums) {
+  yield driver.init(caps);
+  yield drums.chooseFromMainMenu();
+  yield sleep(0.6);
+});
+
+run(function*() {
+  var pianoDriver = yiewd.remote('localhost', 4723);
+  var piano = new Piano(pianoDriver);
+  var drumsDriver = yiewd.remote('localhost', 4725);
+  var drums = new Drums(drumsDriver);
+  yield ll([
+    [initPiano, pianoDriver, piano],
+    [initDrums, drumsDriver, drums]
+  ]);
 
   var director = new Director(96);
   director.addPart(drums, drumScore);
   director.addPart(piano, pianoScore);
   yield director.playScore();
 
-  yield pianoDriver.quit();
-  yield drumsDriver.quit();
+  yield ll([pianoDriver.quit, drumsDriver.quit]);
 });
 
